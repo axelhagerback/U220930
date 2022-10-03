6c948e0a84106a378241ca61031b550d8bb55b40
axelhagerback <axel.hagerback@gmail.com>
2022-10-03 10:05:21 +0200

    first iteration of site completed

---
+++ public/app.js
@@ -0,0 +1,57 @@
+const anka = new XMLHttpRequest();
+
+anka.open('GET', 'structure.json');
+anka.send();
+
+
+const duck = new XMLHttpRequest();
+duck.open('GET', 'cars.json');
+duck.send();
+
+anka.onload = () => {
+
+const keys = JSON.parse(anka.responseText);
+
+for (var key in keys) {
+    console.log(key);
+    
+    var header = document.createElement(key);
+
+    header.innerHTML = (keys[key]);
+
+    main.appendChild(header);
+};
+
+/*
+Make into a forEach later
+
+keys.forEach(key => {
+    var header = document.createElement(key);
+
+    header.innerHTML = (keys[key]);
+
+    main.appendChild(header);
+});*/
+
+};
+
+duck.onload = () => {
+    const cars = JSON.parse(duck.responseText);
+
+    var list = document.createElement('ul');
+    carList.appendChild(list);
+
+    console.log(list);
+
+
+    for (var label in cars) {
+        console.log(label);
+        var listRow = document.createElement('li');
+        listRow.innerHTML = (cars[label]);
+        list.appendChild(listRow);
+    };
+};
+
+loadCars = () => {
+    carList.hidden = false;
+}
