82c3fa06f7be0ea6086323b2972c539c17c50e0c
axelhagerback <axel.hagerback@gmail.com>
2022-10-03 10:53:27 +0200

    refactored code

--- public/app.js
+++ public/app.js
@@ -2,56 +2,32 @@ const structure = new XMLHttpRequest();
 structure.open('GET', 'structure.json');
 structure.send();
 
-
-const duck = new XMLHttpRequest();
-duck.open('GET', 'cars.json');
-duck.send();
+const carList = new XMLHttpRequest();
+carList.open('GET', 'cars.json');
+carList.send();
 
 structure.onload = () => {
-
 const keys = JSON.parse(structure.responseText);
 
 for (var key in keys) {
-    console.log(key);
-    
     var header = document.createElement(key);
-
     header.innerHTML = (keys[key]);
-
     main.appendChild(header);
 };
-
-/*
-Make into a forEach later
-
-keys.forEach(key => {
-    var header = document.createElement(key);
-
-    header.innerHTML = (keys[key]);
-
-    main.appendChild(header);
-});*/
-
 };
 
-duck.onload = () => {
-    const cars = JSON.parse(duck.responseText);
-
+carList.onload = () => {
+    const cars = JSON.parse(carList.responseText);
     var list = document.createElement('ul');
-    carList.appendChild(list);
+    listCars.appendChild(list);
 
     cars.Cars.forEach(car => {
-        console.log(car);
         var listRow = document.createElement('li');
-
         listRow.innerHTML = (`${car.Brand} || ${car.Model} || ${car.Year}`)
-
         list.appendChild(listRow);
     });
-
-
 };
 
 loadCars = () => {
-    carList.hidden = false;
-}
+    listCars.hidden = false;
+};
